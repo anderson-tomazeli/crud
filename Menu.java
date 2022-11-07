50062a5d3b6d028b0a9c566269f85ebf25845333
Anderson Tomazeli <anderson.tomazeli@gmail.com>
2022-11-06 23:31:24 -0300

    List the menu options

---
+++ Menu.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int option;
+
+        do {
+            System.out.println("Choose an operation related to Person/Student:");
+            System.out.println("[1] - Create");
+            System.out.println("[2] - Show");
+            System.out.println("[3] - Update");
+            System.out.println("[4] - Delete");
+            System.out.println("[5] - Exit");
+
+            option = entrada.nextInt();
+        } while (option == 5);
+
+        System.out.println("Bye!");
+        entrada.close();
+    }
+}
